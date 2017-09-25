53df84e673c64e8ddd61682be00406b5f41a8009
Grant Goodman <ggoodfl@gmail.com>
2017-09-24 22:21:15 -0400

    Re-ordered functions in main file

--- JS/angrybirds.js
+++ JS/angrybirds.js
@@ -28,14 +28,6 @@ var scoreText;
 
 var scorePopups = [];
 
-// This disables the slingshot until the scene has loaded
-var simulationStarted = function() {
-
-	scene.removeEventListener( 'update', simulationStarted);
-	window.addEventListener("mousedown", function(e){ handleMouseDown(e); } );
-    	
-}
-
 function init(){
 
 	initScene();
@@ -48,6 +40,209 @@ function init(){
 	initCamera();
 	initRenderer();
 	initAudio();
+
+	requestAnimationFrame( render );
+
+}
+
+function initScene(){
+
+	scene = new Physijs.Scene();
+	scene.setGravity( new THREE.Vector3(0,-100,0) );
+
+	var groundTop = [new THREE.Vector2(0,1), new THREE.Vector2(1,1), new THREE.Vector2(1,0.5), new THREE.Vector2(0,0.5)];
+	var groundSide = [new THREE.Vector2(1,0.5), new THREE.Vector2(1,0), new THREE.Vector2(0,0), new THREE.Vector2(0,0.5)];
+
+	var texture = THREE.ImageUtils.loadTexture('Textures/Grass.png');
+
+	var groundMaterial = Physijs.createMaterial(
+	    new THREE.MeshLambertMaterial({ map: texture }),
+	    1,
+	    0.1
+	);
+
+	var floorGeometry = new THREE.BoxGeometry(2000,4000,18000);
+	floorGeometry.faceVertexUvs[0][4] = [groundTop[0], groundTop[1], groundTop[3]];
+	floorGeometry.faceVertexUvs[0][5] = [groundTop[1], groundTop[2], groundTop[3]];
+
+	floorGeometry.faceVertexUvs[0][0] = [groundSide[0], groundSide[1], groundSide[3]];
+	floorGeometry.faceVertexUvs[0][1] = [groundSide[1], groundSide[2], groundSide[3]];
+
+	var floor = new Physijs.BoxMesh( floorGeometry, groundMaterial, 0 );
+	floor.position.x = -600;
+	floor.position.y = -2000;
+	floor.receiveShadow = true;
+	floor.name = "ground";
+	floor.addEventListener( 'collision', destroyFallingObject, true);
+	scene.add(floor);
+
+	var sideGeometry = new THREE.BoxGeometry(2100,4000,4000);
+	sideGeometry.faceVertexUvs[0][4] = [groundTop[0], groundTop[1], groundTop[3]];
+	sideGeometry.faceVertexUvs[0][5] = [groundTop[1], groundTop[2], groundTop[3]];
+
+	sideGeometry.faceVertexUvs[0][0] = [groundSide[0], groundSide[1], groundSide[3]];
+	sideGeometry.faceVertexUvs[0][1] = [groundSide[1], groundSide[2], groundSide[3]];
+
+	var leftSide = new Physijs.BoxMesh( sideGeometry, groundMaterial, 0 );
+	leftSide.position.z = 10000;
+	leftSide.position.x = -600;
+	leftSide.rotation.x = -Math.PI / 4;
+	leftSide.receiveShadow = true;
+	leftSide.name = "ground";
+	leftSide.addEventListener( 'collision', destroyFallingObject, true);
+	scene.add(leftSide);
+
+	var rightSide = new Physijs.BoxMesh( sideGeometry, groundMaterial, 0 );
+	rightSide.position.z = -10000;
+	rightSide.position.x = -600;
+	rightSide.rotation.x = Math.PI / 4;
+	rightSide.receiveShadow = true;
+	rightSide.name = "ground";
+	rightSide.addEventListener( 'collision', destroyFallingObject, true);
+	scene.add(rightSide);
+
+	generateTargets( -5000, 5400 );
+
+	slingshot = new SLINGSHOT.createSlingshot(scene, new THREE.Vector3(0,100,6100));
+
+}
+
+// This disables the slingshot until the scene has loaded
+var simulationStarted = function() {
+
+	scene.removeEventListener( 'update', simulationStarted);
+	window.addEventListener("mousedown", function(e){ handleMouseDown(e); } );
+    	
+}
+
+function initHud(){
+
+	scoreText = document.createElement('div');
+	scoreText.style.position = 'absolute';
+	scoreText.style.width = 20 + '%';
+	scoreText.style.height = 20 + '%';
+	scoreText.style.color = "white";
+	scoreText.style.fontSize = 3 + 'vw';
+	scoreText.style.top = 5 + '%';
+	scoreText.style.left = 5 + '%';
+	updateScore(0);
+	document.body.appendChild(scoreText);
+
+	var instructions = document.createElement('div');
+	instructions.style.position = 'absolute';
+	instructions.style.width = 20 + '%';
+	instructions.style.height = 20 + '%';
+	instructions.style.color = "white";
+	instructions.style.fontSize = 2 + 'vw';
+	instructions.style.top = 5 + '%';
+	instructions.style.right = 5 + '%';
+	instructions.innerHTML = "Controls:<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
+							 "Aim: Click & Drag<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
+							 "(Anywhere on screen)<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+							 "Fire: Release<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
+							 "Mute: M<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+							 "Press F5 To Generate<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Level";
+	document.body.appendChild(instructions);
+
+	var name = document.createElement('div');
+	name.style.position = 'absolute';
+	name.style.width = 20 + '%';
+	//name.style.height = 20 + '%';
+	name.style.color = "white";
+	name.style.fontSize = 1 + 'vw';
+	name.style.bottom = 1 + '%';
+	name.style.left = 1 + '%';
+	name.innerHTML = "By: Grant Goodman";
+	document.body.appendChild(name);
+
+}
+
+function initLights(){
+
+	baseLight = new THREE.AmbientLight(0x555555);
+	baseLight.intensity = 1;
+	scene.add(baseLight);
+
+	sun = new THREE.DirectionalLight(0xFFFFFF);
+	sun.position = new THREE.Vector3( 1, 1, 0 );
+	sun.intensity = 1;
+	sun.shadowCameraNear = 0.1;
+	sun.shadowCameraFar = 1000;
+	sun.castShadow = true;
+	scene.add(sun);
+
+}
+
+function initCamera(){
+
+	camera = new BIRD.BirdCamera( 3500, new THREE.PerspectiveCamera( 45, 1, 0.1, 10000 ), slingshot.handleMesh );
+	camera.camera.lookAt( scene.position );
+
+}
+
+function initRenderer(){
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor( 0xAAAAFF, 1.0 );
+	
+	renderer.shadowMapEnabled = true;
+
+	resize();
+
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function initAudio(){
+
+	// Found at https://freesound.org/people/devlab/sounds/170515/
+	ambientNoise = new Audio('Sounds/ambient_forest.wav');
+	ambientNoise.loop = true;
+	ambientNoise.play();
+
+	// Found at https://freesound.org/people/frankum/sounds/393520/
+	music = new Audio('Sounds/guitar_loop.mp3');
+	music.loop = true;
+	music.play();
+
+	// Found at https://freesound.org/people/kevinkace/sounds/66777/
+	breakNoise = new Audio('Sounds/wood_break.wav');
+
+	// Found at https://freesound.org/people/Planman/sounds/208111/
+	poof = new Audio('Sounds/poof.wav');
+
+	window.addEventListener('keydown', function(event) {
+		if(event.keyCode == 77){
+			mute = !mute;
+			if(mute){
+				ambientNoise.pause();
+				music.pause();
+			}else{
+				ambientNoise.play();
+				music.play();
+			}
+		}
+	}, false);
+
+}
+
+function render(){
+
+	if( bird != null )
+		if( bird.timeToDelete() )
+			deleteBird();
+
+	for(var i = 0; i < scorePopups.length; i++){
+
+		scorePopups[i].position.y += 3;
+
+	}
+
+	scene.simulate();
+
+	camera.updateCamera();
+
+	renderer.render( scene, camera.camera );
 
 	requestAnimationFrame( render );
 
@@ -78,6 +273,7 @@ function newScorePopup( textContent, position ){
 	setTimeout(
 		function(){
 			scene.remove(text);
+			scorePopups.splice(scorePopups.indexOf(text),1);
 		},
 		3000
 	);
@@ -86,77 +282,57 @@ function newScorePopup( textContent, position ){
 
 }
 
-function initScene(){
+function resize() {
+	const w = document.body.clientWidth;
+	const h = document.body.clientHeight;
+    renderer.setSize(w, h);
+    camera.camera.aspect = w / h;
+    camera.camera.updateProjectionMatrix();
+}; 
 
-	scene = new Physijs.Scene();
-	scene.setGravity( new THREE.Vector3(0,-100,0) );
+window.addEventListener("resize", resize);
 
-	var groundTop = [new THREE.Vector2(0,1), new THREE.Vector2(1,1), new THREE.Vector2(1,0.5), new THREE.Vector2(0,0.5)];
-	var groundSide = [new THREE.Vector2(1,0.5), new THREE.Vector2(1,0), new THREE.Vector2(0,0), new THREE.Vector2(0,0.5)];
+function handleMouseDown(event){
 
-	var texture = THREE.ImageUtils.loadTexture('Textures/Grass.png');
+	if( camera.parent.name == "slingshot" ){
+		mouseDownPos = new THREE.Vector2( event.clientX, event.clientY );
+		if(!mute)
+			slingshot.stretch.play();
 
-	var groundMaterial = Physijs.createMaterial(
-	    new THREE.MeshLambertMaterial({ map: texture }),
-	    1,
-	    0.1
-	);
 
-	var destroyFallingObject = function(other_object, relative_velocity, relative_rotation, contact_normal){
+		window.addEventListener("mousemove", handleMouseMovement);
+		window.addEventListener("mouseup", handleMouseUp);
+	}
 
-		if( other_object.name == "Falling" ){
-			if(!mute)
-				poof.play();
-			newScorePopup(100,other_object.position);
-			updateScore(100);
-			remove(other_object);
-		}
+}
+
+var handleMouseMovement = function( e ){
+
+	slingshot.updateIndicator((mouseDownPos.x - e.clientX)/(document.body.clientWidth), (mouseDownPos.y - e.clientY)/(window.innerHeight));
+
+}
+
+var handleMouseUp = function( e ){
+
+	if( (e.clientX - mouseDownPos.x)/(window.innerHeight) < 0 ){
+		bird = new BIRD.createBird( 0xFF0000, scene, new THREE.Vector3(0,400,6100) );
+		bird.mesh.addEventListener( 'collision', destroyDestructibleObject, true);
+		camera.setParent( bird.mesh );
+
+		bird.mesh.setLinearVelocity(
+			new THREE.Vector3(
+				0,
+				speed * (e.clientY - mouseDownPos.y)/(document.body.clientHeight),
+				speed * (e.clientX - mouseDownPos.x)/(document.body.clientWidth)
+			)
+		);
 
 	}
 
-	var floorGeometry = new THREE.BoxGeometry(2000,4000,18000);
-	floorGeometry.faceVertexUvs[0][4] = [groundTop[0], groundTop[1], groundTop[3]];
-	floorGeometry.faceVertexUvs[0][5] = [groundTop[1], groundTop[2], groundTop[3]];
+	slingshot.resetIndicator(mute);
 
-	floorGeometry.faceVertexUvs[0][0] = [groundSide[0], groundSide[1], groundSide[3]];
-	floorGeometry.faceVertexUvs[0][1] = [groundSide[1], groundSide[2], groundSide[3]];
-
-	var floor = new Physijs.BoxMesh( floorGeometry, groundMaterial, 0 );
-	floor.position.x = -600;
-	floor.position.y = -2000;
-	floor.receiveShadow = true;
-	floor.name = "ground";
-	floor.addEventListener( 'collision', destroyFallingObject);
-	scene.add(floor);
-
-	var sideGeometry = new THREE.BoxGeometry(2100,4000,4000);
-	sideGeometry.faceVertexUvs[0][4] = [groundTop[0], groundTop[1], groundTop[3]];
-	sideGeometry.faceVertexUvs[0][5] = [groundTop[1], groundTop[2], groundTop[3]];
-
-	sideGeometry.faceVertexUvs[0][0] = [groundSide[0], groundSide[1], groundSide[3]];
-	sideGeometry.faceVertexUvs[0][1] = [groundSide[1], groundSide[2], groundSide[3]];
-
-	var leftSide = new Physijs.BoxMesh( sideGeometry, groundMaterial, 0 );
-	leftSide.position.z = 10000;
-	leftSide.position.x = -600;
-	leftSide.rotation.x = -Math.PI / 4;
-	leftSide.receiveShadow = true;
-	leftSide.name = "ground";
-	leftSide.addEventListener( 'collision', destroyFallingObject);
-	scene.add(leftSide);
-
-	var rightSide = new Physijs.BoxMesh( sideGeometry, groundMaterial, 0 );
-	rightSide.position.z = -10000;
-	rightSide.position.x = -600;
-	rightSide.rotation.x = Math.PI / 4;
-	rightSide.receiveShadow = true;
-	rightSide.name = "ground";
-	rightSide.addEventListener( 'collision', destroyFallingObject);
-	scene.add(rightSide);
-
-	generateTargets( -5000, 5400 );
-
-	slingshot = new SLINGSHOT.createSlingshot(scene, new THREE.Vector3(0,100,6100));
+	window.removeEventListener("mousemove", handleMouseMovement);
+	window.removeEventListener("mouseup", handleMouseUp);
 
 }
 
@@ -211,51 +387,22 @@ function generateStepStair(numSteps, z, lastY){
 
 }
 
-function initHud(){
-
-	scoreText = document.createElement('div');
-	scoreText.style.position = 'absolute';
-	scoreText.style.width = 20 + '%';
-	scoreText.style.height = 20 + '%';
-	scoreText.style.color = "white";
-	scoreText.style.fontSize = 3 + 'vw';
-	scoreText.style.top = 5 + '%';
-	scoreText.style.left = 5 + '%';
-	updateScore(0);
-	document.body.appendChild(scoreText);
-
-	var instructions = document.createElement('div');
-	instructions.style.position = 'absolute';
-	instructions.style.width = 20 + '%';
-	instructions.style.height = 20 + '%';
-	instructions.style.color = "white";
-	instructions.style.fontSize = 2 + 'vw';
-	instructions.style.top = 5 + '%';
-	instructions.style.right = 5 + '%';
-	instructions.innerHTML = "Controls:<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
-							 "Aim: Click & Drag<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
-							 "Fire: Release<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
-							 "Mute: M<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
-							 "Press F5 To Generate<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Level";
-	document.body.appendChild(instructions);
-
-	var name = document.createElement('div');
-	name.style.position = 'absolute';
-	name.style.width = 20 + '%';
-	//name.style.height = 20 + '%';
-	name.style.color = "white";
-	name.style.fontSize = 1 + 'vw';
-	name.style.bottom = 1 + '%';
-	name.style.left = 1 + '%';
-	name.innerHTML = "By: Grant Goodman";
-	document.body.appendChild(name);
-
-}
-
 function updateScore(amount){
 
 	score += amount;
 	scoreText.innerHTML = "Score: " + score;
+
+}
+
+var destroyFallingObject = function(other_object, relative_velocity, relative_rotation, contact_normal){
+
+	if( other_object.name == "Falling" ){
+		if(!mute)
+			poof.play();
+		newScorePopup(100,other_object.position);
+		updateScore(100);
+		remove(other_object);
+	}
 
 }
 
@@ -288,148 +435,5 @@ function deleteBird(){
 function remove(name){
 
 	scene.remove( name );
-
-}
-
-function initLights(){
-
-	baseLight = new THREE.AmbientLight(0x555555);
-	baseLight.intensity = 1;
-	scene.add(baseLight);
-
-	sun = new THREE.DirectionalLight(0xFFFFFF);
-	sun.position = new THREE.Vector3( 1, 1, 0 );
-	sun.intensity = 1;
-	sun.shadowCameraNear = 0.1;
-	sun.shadowCameraFar = 1000;
-	sun.castShadow = true;
-	scene.add(sun);
-
-}
-
-function initRenderer(){
-
-	renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor( 0xAAAAFF, 1.0 );
-	
-	renderer.shadowMapEnabled = true;
-
-	resize();
-
-	document.body.appendChild( renderer.domElement );
-
-}
-
-function resize() {
-	const w = document.body.clientWidth;
-	const h = document.body.clientHeight;
-    renderer.setSize(w, h);
-    camera.camera.aspect = w / h;
-    camera.camera.updateProjectionMatrix();
-}; 
-
-window.addEventListener("resize", resize);
-
-function handleMouseDown(event){
-
-	if( camera.parent.name == "slingshot" ){
-		mouseDownPos = new THREE.Vector2( event.clientX, event.clientY );
-		if(!mute)
-			slingshot.stretch.play();
-
-
-		window.addEventListener("mousemove", handleMouseMovement);
-		window.addEventListener("mouseup", handleMouseUp);
-	}
-
-}
-
-var handleMouseMovement = function( e ){
-
-	slingshot.updateIndicator((mouseDownPos.x - e.clientX)/(document.body.clientWidth), (mouseDownPos.y - e.clientY)/(window.innerHeight));
-
-}
-
-var handleMouseUp = function( e ){
-
-	if( (e.clientX - mouseDownPos.x)/(window.innerHeight) < 0 ){
-		bird = new BIRD.createBird( 0xFF0000, scene, new THREE.Vector3(0,400,6100) );
-		bird.mesh.addEventListener( 'collision', destroyDestructibleObject);
-		camera.setParent( bird.mesh );
-
-		bird.mesh.setLinearVelocity(
-			new THREE.Vector3(
-				0,
-				speed * (e.clientY - mouseDownPos.y)/(document.body.clientHeight),
-				speed * (e.clientX - mouseDownPos.x)/(document.body.clientWidth)
-			)
-		);
-
-	}
-
-	slingshot.resetIndicator(mute);
-
-	window.removeEventListener("mousemove", handleMouseMovement);
-	window.removeEventListener("mouseup", handleMouseUp);
-
-}
-
-function initCamera(){
-
-	camera = new BIRD.BirdCamera( 3500, new THREE.PerspectiveCamera( 45, 1, 0.1, 10000 ), slingshot.handleMesh );
-	camera.camera.lookAt( scene.position );
-
-}
-
-function initAudio(){
-
-	ambientNoise = new Audio('Sounds/ambient_forest.wav');
-	ambientNoise.loop = true;
-	ambientNoise.play();
-
-	music = new Audio('Sounds/guitar_loop.mp3');
-	music.loop = true;
-	music.play();
-
-	// Found at https://freesound.org/people/kevinkace/sounds/66777/
-	breakNoise = new Audio('Sounds/wood_break.wav');
-
-	// Found at https://freesound.org/people/Planman/sounds/208111/
-	poof = new Audio('Sounds/poof.wav');
-
-	window.addEventListener('keydown', function(event) {
-		if(event.keyCode == 77){
-			mute = !mute;
-			if(mute){
-				ambientNoise.pause();
-				music.pause();
-			}else{
-				ambientNoise.play();
-				music.play();
-			}
-		}
-	}, false);
-
-}
-
-function render(){
-
-	if( bird != null )
-		if( bird.timeToDelete() )
-			deleteBird();
-
-	for(var i = 0; i < scorePopups.length; i++){
-
-		scorePopups[i].position.y += 3;
-
-	}
-
-	scene.simulate();
-
-	camera.updateCamera();
-
-	renderer.render( scene, camera.camera );
-
-	requestAnimationFrame( render );
 
 }
